7bfd1790104f56cee8443d1809c3a260ec0a70b9
Pat Marshall <pmarshall1716@gmail.com>
2016-06-15 22:43:24 -0600

    root est., css styling., grid creation

---
+++ app/assets/javascripts/memory.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$('.grid-cell').click(function(){
+		if($(this).hasClass('green-cell')){
+			$(this).text('').removeClass('green-cell');
+		}
+		else {
+			$(this).text('clicked').addClass('green-cell');
+		}
+});
